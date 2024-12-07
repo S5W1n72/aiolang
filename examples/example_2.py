9c1ab29c41acf613553f4f2b83e28c159321d2a4
stone <menu.oxg@gmail.com>
2024-12-07 21:23:12 +0330

    Update example_2.py
    Signed-off-by: stone <menu.oxg@gmail.com>

--- examples/example_2.py
+++ examples/example_2.py
@@ -1,10 +1,13 @@
 import asyncio
-from aiolang import Translate
+from aiolang import Aiolang, TranslationError
 
 async def main():
-    translate = Translate()
-    request = await translate("Hello, World!", "KO")
-    print(request)
+    aiolang = Aiolang()
+    try:
+        result = await aiolang.translate_text("hello", "fa")
+        print(result)
+    except TranslationError as log:
+        log.display_error()
 
 if __name__ == "__main__":
     asyncio.run(main())
